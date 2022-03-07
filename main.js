3e3c628ce5d2763366e90297a55a83e16e140093
amanc1248 <amanc1248@gmail.com>
2022-03-07 17:40:06 +0545

    ADD: mutate method

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
+  const dnaBases = ["A", "T", "C", "G"];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
@@ -13,10 +13,23 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+//
+const pAequorFactory = (num, dnaArray) => {
+  return {
+    specimenNum: num,
+    dna: dnaArray,
+    mutate() {
+      const randIndex = Math.floor(Math.random() * this.dna.length);
+      let newBase = returnRandBase();
 
+      while (newBase === this.dna[randIndex]) {
+        newBase = returnRandBase();
+      }
+      this.dna[randIndex] = newBase;
+    },
+    compareDNA(pAequor) {},
+  };
+};
 
-
-
-
-
-
+console.log(returnRandBase());
+console.log(mockUpStrand());
